9510ea3da7cbbf0ac67642f4b537ba6a65ea2902
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-11-07 16:10:19 +0700

    controller

---
+++ models/posting.js
@@ -0,0 +1,20 @@
+const Mongoose = require('mongoose');
+const Schema = Mongoose.Schema;
+
+const PostSchema = new Schema({
+  image_url: String,
+  caption: String,
+  Likes: [{
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  }]  
+}, { timestamps: true })
+
+PostSchema.pre('save', function(next){
+  this.Likes = [];
+  next()
+})
+
+const Post = Mongoose.model('posts', PostSchema);
+
+module.exports = Post;
